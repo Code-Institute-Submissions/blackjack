51c0139946d6911c24426d26c6932a98c74e03de
Damian Rodbari <skullphish@gmail.com>
2018-08-12 17:55:05 +0000

    rescaled the PNGs, data held in session tidied up, reseting function added to reset the table and clear the session, more comments added, Deck class has a new method called reset . each player plays agasint the house using only 1 set of deck to make card counting possible. the counting function is now smarter when it comes to dealing with aces.

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-import copy
+import time # use with time.sleep(5) 5 sec delay
 import sys
 from flask import Flask, render_template, request, redirect, flash, url_for, render_template_string, session
-from pickle_manage_data import *
 from flask_wtf import Form, FlaskForm
 from wtforms import StringField, IntegerField, IntegerField, SubmitField, SelectField   # http://wtforms.simplecodes.com/docs/0.6.2/fields.html
 from wtforms.validators import DataRequired
-from game import Deck
+from game import *
 
 # from werkzeug.datastructures import ImmutableMultiDict
 
@@ -14,17 +13,33 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = os.urandom(24)
 
-# where the entire data is held
-# game_state = dict()
 
-house = Deck(2) # number of players going in
-print(house.deck)
+
+def reset_table(restart=False):
+    
+    """ if the game was reset during the game then clear cash, 
+        otherwise just re-set the table and deal new cards to
+        both player and house """
+        
+    if restart:
+        session.clear()     # gets rid of the lingering data but clearing the session
+        
+    deck.reset()       # restores a full deck of cards
+    session["houseHand"] = deck.deal(2) # list( deck.deal() for x in range(2) )
+    session["playerHand"] = deck.deal(2) # list( deck.deal() for x in range(2) )
+
+# defining globals
+deck = Deck() # number of players going in
+
+
+
+# houseHand = list( house.deal() for x in range(2) )
+# houseHandValue = 0
+# print(house.deck)
 # print(len(house.deck))
-# print(house.deal())
-# print(house.deal())
-# print(house.deal())
-# print(house.deal())
-# print(len(house.deck))
+# test_card = house.deal()
+# print(test_card)
+# print(convertCardNames(test_card))
 
 # game idea
 # a bastardised version of blackjack, where players take turn to play against the house,
@@ -34,20 +49,24 @@ print(house.deck)
 # of players set. 2 decks by default
 # how to add images of cards on the screen?
 
+    
+    # game logic before player decides on hit or stand
+    # the house must show 2 cards
+    # possible variables
+        # house_hand = house cards shown or to be shown
+        # player_hand = player cards 
 
+    
+    
+    # if HIT pressed append another card 
+    
+    # if STAND hit:
+        # calculate player hand
+        # house.deal()
+    
+    # players
+    # if player.hand value is greater than 18, the STAND button should pulsate
 
-# TO ASK!
-
-# why cant i install "flask_wtf"? - RESOLVED!
-    # i am better off using flask_wtf - tutorials online are very helpful
-# still not clear on sessions, users, how the data is held! does refresh gets rid of everything?
-    # the difference between sessions and pickle:
-        # dont we write to the disk using pickle? why do we have to use it at the first place?
-        # does it load user data? if it does how does it pull it off? how do i do it?
-# how do i pass arguments in with redirect using url_for? 
-# how do i use SVG cards, it's only one file containing all the cards
-        # how link the images to the format used to contruct the deck ("suit", "")
-# player class needs to be added - player.deck() ...
 
 #  ================= routes =====================
 @app.route("/base")
@@ -55,13 +74,16 @@ def base():
     return render_template("base.html")
 
 
+# Welcome view
 @app.route("/", methods=["POST","GET"])
 def index():
-
+    
+    reset_table() # start fresh by clearing the session, resetting the deck
+    
     if request.method == "POST":
     
         # the data extracted from the form was coming in as an "ImmutableMultiDict", which made it absolutely impossible to
-        # to use the same names (name = username) on the form inputs as it was only capable getting the first value out,
+        # to use the same names (name = username) on the form inputs, as it was only capable getting the first value out,
         # it simply fetched the first value with the key "username" and it ignored the rest. However, i wished to use the
         # same names for the inputs within my form however unconventional it might be. so with the use of ".getlist()" the 
         #"ImmutableMultiDict" was changed to a normal dictionary which stores the values for my username key (name input on the form) 
@@ -70,7 +92,7 @@ def index():
         # please refer to the readme.md for more information on this
         for key in request.form.keys():
             
-            # game_state[key] = request.form.getlist(key)
+            # fetching the initital user data out of the from and putting it in a normal dictionary
             session[key] = request.form.getlist(key)
             
             if key == "username":
@@ -79,63 +101,110 @@ def index():
                 session["score"] = {username: 0 for username in session["username"]}
             
             if key == "rounds":
-                # targeting the only value within the list and converting it to integer
+                # sets how many rounds a single game would last.
+                # will be used as an integer from this point onwards
                 session["rounds"] = int(session["rounds"][0]) 
         
-        # will be used in conjuction with the given usernames, allowing the 
-        # usernames  to play their turns in the same order as they were submitted.     
-        session["seq"] = 0 
-        session["rounds_played"] = 0 # to keep track of the rounds played
-        session["won"] = "TBC"  # winner to be declared
-
+        # expanding on the uses data held in session
+        session["seq"] = 0              # player turn tracker
+        session["rounds_played"] = 0    # round tacker
+        session["won"] = "TBC"          # winner to be declared
+        session["status"] = "TBC"       # bust, push, blackjack - need two? for player and house
+        
+        # by this point all the basic player data is now stored in the session.
         print("session = ", session)
-
-        return redirect( url_for('game') ) # targets the view(function)
+        
+        # if POST then redirect to the first player in game view
+        return redirect( url_for('game') )
         
     return render_template("index.html")
     
 
+# game page
 @app.route("/game", methods=["POST","GET"])
 def game():
-
-    scores = session["score"]
-    usernames = session["username"]
-
-    seq = session["seq"] # assigned to a variable only for readability purposes
-    username = session["username"][seq]
     
+    scores = session["score"]               # holds all the scores
+    usernames = session["username"]         # holds all the usernames 
+    seq = session["seq"]                    # assigned to a variable only for readability purposes
+    username = session["username"][seq]     # player whose turn it is to play
+    
+
     if request.method == "POST":
         
         print("request.form.keys() = ", request.form.keys())
         
-
+        if "reset" in request.form.keys():
+            # reseting mechanism - triggered if reset button is pressed during an action session of the game
+            reset_table(restart=True)   # clearing the session as well as resetting the table 
+            return redirect( url_for("index")) 
         
-        if "win" in request.form.keys():
-            scores[username] += 1
+        if "hit" in request.form.keys():
+            # deals one card to the player
+            # session["houseHand"].append(house.deal())
+            session["playerHand"].append(deck.deal())
+        
+        # if player goes "BUST" - increment seq key to move on the next player
+        if getHandValue( session["playerHand"] )[1] == "BUST":
+            print("BUUUUUUUUUUSSSSSTTT")
+            session["seq"] += 1
+            reset_table()
+        
+        # if the player gets "blackjack"
+        if getHandValue( session["playerHand"] )[1] == "BLACKJACK":
+            print("BLACKJACKKKKKKKKKKKKKKKKKKKK")
             
-        session["score"] = scores
-        print("scores = ", scores)
-        session["seq"] += 1
+            # house has to play after the player so this bit of code needs to move!
+                # if house goes bust:       Decalre "BLACKJACK" - increment score and seq
+                # if house get blackjack:   declare "PUSH" - increment seq ONLY!  
+            
+            scores[username] += 1
+            session["seq"] += 1
+            reset_table()
+            
+            
+        # if the player wins its turn, award one point 
+        # if "win" in request.form.keys():
+        #     scores[username] += 1
         
-        # reseting mechanism - simulating a full cycle
+        session["score"] = scores   # Aliased since session resets the content of the list to zero
+        print("scores = ", scores)
+        
+        # if "hit" not in request.form.keys():
+        #     session["seq"] += 1         # win or lose, increment the sequence counter for the next player to start its turn
+        
+        # reseting mechanism - simulating a full cycle - each full cycle means 1 round
         if session["seq"] >= len( session["username"] ):
-            session["seq"] = 0            # reset the counter to cycle to the first player
-            session["rounds_played"] += 1 # one cycle is complete meaning one round has been played!
+            session["seq"] = 0              # reset the sequence counter to cycle back to the first player
+            session["rounds_played"] += 1   # when a cycle is complete, one found of game has been played by all the player!
             
             print("rounds played = ", session["rounds_played"])
             
-
+            # declare the winner
             if session["rounds_played"] == session["rounds"]:
+                
+                # select the player with the heighest score at the end of the final "pre-defined" round.
                 session["won"] = max(scores, key= lambda x: scores[x])
                 return redirect( url_for('winner') ) 
+    
 
-    # print("NO POST: username= {}, seq= {}".format(username, session["seq"]))
-    return render_template("game.html", usernames=usernames, scores=scores, seq=session["seq"], round=session["rounds_played"]+1)
+    player_dict = {"hand": convertCardNames( session["playerHand"] ), "value" : getHandValue( session["playerHand"] ) }
+    house_dict = {"hand": convertCardNames( session["houseHand"] ), "value" : getHandValue( session["playerHand"] ) }
+    round_dict = {"total": session["rounds"], "played": session["rounds_played"]+1}
+
+    return render_template("game.html", usernames=usernames, scores=scores, seq=session["seq"], 
+        rounds=round_dict, player=player_dict, house=house_dict)
     
 
 
 @app.route("/winner")
 def winner():
+    
+    # note:
+    # the scenario at which two players have the same score at the end 
+    # of the final round needs to be address
+    #   decalre both as winners? raise the the rounds by 3?
+    # session.pop(key)
     return render_template("winner.html", winner=session["won"])
 
 if __name__ == "__main__":
--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import sys 
 import random 
 import itertools
 
@@ -5,34 +6,137 @@ class Deck:
     """ Deck of playing cards """
 
 
-    def __init__(self, nop):
+    def __init__(self):
         
         self.suits = ["spades", "clubs", "hearts", "diamonds"]
         self.ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        self.nop = nop    # Number Of Players cgange to num_of_players
-        self.deck = list(itertools.product(self.suits, self.ranks)) * self.nop
+        self.deck = list(itertools.product(self.suits, self.ranks))
         random.shuffle(self.deck)
     
 
-    def deal(self):
+    def deal(self, num=1):
         """ deal a card and remove that card from the deck, 
-            this will also serve as a HIT function """
-        card = self.deck.pop() # since the deck is already shuffled we can just use pop 
-        return card
+            this will also serve as a HIT function 
+            num:    number of cards being drawn
+        """
+            
+        if num == 1:
+            return self.deck.pop() # since the deck is already shuffled we can just use pop
+        else:
+            return list( self.deck.pop() for x in range(num) )
 
 
     def __str__(self):
         return "test"
     
-    
-    # how to reset the deck when a player wins?
+    def reset(self):
+        """ restore the deck to its original state "52" cards """        
+        
+        self.deck = list(itertools.product(self.suits, self.ranks))
+        random.shuffle(self.deck)
+        print("deck is now reset to {} cards".format( len(self.deck) ))
+
 
 class Player:
     """ Simulate a player """
     
-    def __init__(self,name, score):
+    def __init__(self,name, score, hand=None):
+        if hand is None:
+            self.hand = []
+        else:
+            self.hand = hand
         self.name = name
         self.score = score
         
     def __str__(self):
         return "Player" 
+    
+
+def convertCardNames(cards):
+    
+    
+    """ 
+    convert the name of the cards in the deck to the names of 
+    the individual PNGs.
+    card names in deck: ('spades', 'Q') 
+    target name: Q_of_spades.png
+    """
+    name = list(card[1] + "_of_" + card[0] + ".png" for card in cards)
+        
+    return name
+    
+    
+    
+def getHandValue(hand):
+    
+    # K,Q,J are worth 10 points each
+    # A is worth either 1 or 11 depending on the hand
+    # 2-10 = face value
+    # format = ('spades', 'K')
+    
+    jdebug = 0
+    if jdebug > 0:  print( "count_hand() called by: {}".format(sys._getframe(1).f_code.co_name) )
+    
+    val = 0
+    aceCounter = 0
+    aceCounterAddressed = 0
+    for card in hand:
+        if jdebug > 0:  print("card={} card[1]={}".format(card, card[1]))
+        
+        try:
+            val += int(card[1])
+            if jdebug > 0:  print("Number detected, {}".format(card[1]))
+            
+        except ValueError:
+            
+            if jdebug > 0:  print("Letter detected, {}".format(card[1]))
+            
+            if card[1] in ["K", "Q", "J"]:
+                val += 10
+            
+            else:
+                if jdebug > 0:  print("'A' detected, {}".format(card[1]))
+                
+                if val+11 > 21:
+                    val += 1
+                else:
+                    aceCounter += 1
+                    val += 11
+                
+        if aceCounter > 0 and val > 21 and (aceCounterAddressed != aceCounter):
+            print("special! aceCounter, val = ", aceCounter, val)
+            aceCounterAddressed += 1
+            val -= 10
+        
+        if val == 21:
+            status = "BLACKJACK"
+        elif val > 21:
+            status = "BUST"
+        else:
+            status = "active"
+        
+        
+    return val, status
+
+
+def getVerdict(*hands):
+    
+    """
+    decides who wins
+    hand[0] = player
+    hand[1] = house
+    """
+    
+    verdict = "undecided"
+    
+    # if player goes bust or house gets blackjack decale house as winner
+    if hands[0] == "BUST" or hands[1] == "BLACKJACK":
+        verdict = "house"
+        
+    # if house goes bust or player gets blackjack decale player as winner
+    elif hands[1] == "BUST" or hands[0] == "BLACKJACK":
+        verdict = "player"
+    
+    # if both player and house get black jack then PUSH
+    if hands[0] == "BLACKJACK" and hands[1] == "BLACKJACK":
+        verdict = "PUSH"
